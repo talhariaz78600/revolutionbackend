79e748a1e9330d2a7bd3924bad70467e38b0521a
Talha Riaz <muhammadtalha1005@gmail.com>
2024-06-01 14:48:59 +0500

    yahoo

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -5,19 +5,29 @@ const secretID = process.env.secret_ID_JWT
 const jwt = require('jsonwebtoken');
 // const bcrypt = require('bcrypt');
 router.get("/login/success", async(req, res) => {
-	if (req.user) {
-		console.log(req.user);
-		const newuser = new User({firstname:req.user.name.givenName,lastname:req.user.name.familyName,email:req.user.emails.value})
-        newuser.status = true;
-        jwt.sign({ id: newuser._id }, secretID, { expiresIn: '30d' }, async (err, UserToken) => {
-            newuser.sessionExpiration = new Date().getTime() + (1000 * 60 * 60 * 24 * 30); // 30 days in milliseconds
-            newuser.jwttoken = UserToken;
-            await newuser.save();
-            res.status(200).json({ message: 'Successfully Sign In', newuser });
-        });
-	} else {
-		res.status(403).json({ error: true, message: "Not Authorized" });
+	try {
+		if (!req.user) {
+			res.status(400).json({message:"Invalid request"})
+		}
+
+		const newUser=await User.findOne({email:req.user.emails.value})
+		if(!newUser){
+			console.log(req.user);
+			const newuser = new User({firstname:req.user.name.givenName,lastname:req.user.name.familyName,email:req.user.emails.value})
+			newuser.status = true;
+			jwt.sign({ id: newuser._id }, secretID, { expiresIn: '30d' }, async (err, UserToken) => {
+				newuser.sessionExpiration = new Date().getTime() + (1000 * 60 * 60 * 24 * 30); // 30 days in milliseconds
+				newuser.jwttoken = UserToken;
+				await newuser.save();
+				res.status(200).json({ message: 'Successfully Sign In', newuser });
+			});
+
+		}
+		res.status(200).json({newuser:newUser})
+	} catch (error) {
+		res.status(500).json({message:"internel server error",errors:error.message})
 	}
+	
 });
 
 router.get("/login/failed", (req, res) => {
