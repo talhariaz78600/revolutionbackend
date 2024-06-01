5d02ffc189b73fb71591f14762437d18f5664128
Talha Riaz <muhammadtalha1005@gmail.com>
2024-06-01 15:49:35 +0500

    lastchance

--- passport.js
+++ passport.js
@@ -1,23 +1,53 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const { response } = require("express");
 const passport = require("passport");
-
+const User=require("./models/Mongoousers")
 passport.use(
 	new GoogleStrategy(
-		{
-			clientID: `${process.env.CLIENT_ID}`,
-			clientSecret:`${process.env.CLIENT_SECRET}`,
-			callbackURL: "https://revolutionbackend.vercel.app/auth/google/callback",
-			scope: ["profile", "email"],
-		},
-		function (accessToken, refreshToken, profile, callback) {
-			console.log('Profile Data')
-			console.log(profile)
-			// response.json({profile})
-			callback(null, profile);
+	  {
+		clientID: process.env.CLIENT_ID,
+		clientSecret: process.env.CLIENT_SECRET,
+		callbackURL: 'https://revolutionbackend.vercel.app/auth/google/callback',
+		scope: ['profile', 'email'],
+	  },
+	  async (accessToken, refreshToken, profile, callback) => {
+		try {
+		  const existingUser = await User.findOne({ googleId: profile.emails.value });
+  
+		  if (existingUser) {
+			// User already exists in the database
+			return callback(null, existingUser);
+		  }
+  
+		  // User does not exist, create a new user
+		  const newUser = new User({
+			
+			firstname: profile.name.givenName,
+			lastname: profile.name.familyName,
+			email: profile.emails.value,
+		  });
+  
+		  await newUser.save();
+		  return callback(null, newUser);
+		} catch (error) {
+		  return callback(error, null);
 		}
+	  }
 	)
-);
+  );
+  
+  passport.serializeUser((user, done) => {
+	done(null, user.id);
+  });
+  
+  passport.deserializeUser(async (id, done) => {
+	try {
+	  const user = await User.findById(id);
+	  done(null, user);
+	} catch (error) {
+	  done(error, null);
+	}
+  });
 
 passport.serializeUser((user, done) => {
 	done(null, user);
--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -1,46 +1,18 @@
 const router = require("express").Router();
 const passport = require("passport");
-const User=require("../models/Mongoousers")
-const secretID = process.env.secret_ID_JWT
-const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcrypt');
-router.get('/login/success', async (req, res) => {
-	try {
-	//   if (!req.user) {
-		return res.status(200).json({ message: 'getsuccessfully',user:req.user});
-	//   }
-  
-	//   const userEmail = req.user.emails.value; // Correctly accessing the email value
-	//   let existingUser = await User.findOne({ email: userEmail });
-  
-	//   if (!existingUser) {
-	// 	console.log(req.user);
-  
-	// 	const newUser = new User({
-	// 	  firstname: req.user.name.givenName,
-	// 	  lastname: req.user.name.familyName,
-	// 	  email: userEmail,
-	// 	  status: true,
-	// 	});
-  
-	// 	jwt.sign({ id: newUser._id }, secretID, { expiresIn: '30d' }, async (err, UserToken) => {
-	// 	  if (err) {
-	// 		return res.status(500).json({ message: 'Token generation failed', errors: err.message });
-	// 	  }
-  
-	// 	  newUser.sessionExpiration = new Date().getTime() + (1000 * 60 * 60 * 24 * 30); // 30 days in milliseconds
-	// 	  newUser.jwttoken = UserToken;
-	// 	  await newUser.save();
-  
-	// 	  return res.status(200).json({ message: 'Successfully signed in', newuser: newUser });
-	// 	});
-	//   } else {
-	// 	return res.status(200).json({ newuser: existingUser });
-	//   }
-	} catch (error) {
-	  return res.status(500).json({ message: 'Internal server error', errors: error.message });
+// const User=require("../models/")
+router.get("/login/success", (req, res) => {
+	if (req.user) {
+		console.log(req.user);
+		res.status(200).json({
+			error: false,
+			message: "Successfully Loged In",
+			user: req.user,
+		});
+	} else {
+		res.status(403).json({ error: true, message: "Not Authorized" });
 	}
-  });
+});
 
 router.get("/login/failed", (req, res) => {
 	res.status(401).json({
