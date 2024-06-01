5b3aa4186d0861376d8a499be7bc07981095f0e3
Talha Riaz <muhammadtalha1005@gmail.com>
2024-06-01 14:20:19 +0500

    wow

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -1,14 +1,20 @@
 const router = require("express").Router();
 const passport = require("passport");
-
+const User=require("../models/Mongoousers")
+const secretID = process.env.secret_ID_JWT
+const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt');
 router.get("/login/success", (req, res) => {
 	if (req.user) {
 		console.log(req.user);
-		res.status(200).json({
-			error: false,
-			message: "Successfully Loged In",
-			user: req.user,
-		});
+		const newuser = new User({firstname:req.user.name.givenName,lastname:req.user.name.familyName,email:req.user.emails.value})
+        newuser.status = true;
+        jwt.sign({ id: newuser._id }, secretID, { expiresIn: '30d' }, async (err, UserToken) => {
+            newuser.sessionExpiration = new Date().getTime() + (1000 * 60 * 60 * 24 * 30); // 30 days in milliseconds
+            newuser.jwttoken = UserToken;
+            await newuser.save();
+            res.status(200).json({ message: 'Successfully Sign In', newuser });
+        });
 	} else {
 		res.status(403).json({ error: true, message: "Not Authorized" });
 	}
