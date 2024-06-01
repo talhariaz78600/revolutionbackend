3215a9d3127e7fd8ad9ae2ecbc99032fa2901da5
Talha Riaz <muhammadtalha1005@gmail.com>
2024-06-01 15:19:44 +0500

    long

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -6,37 +6,37 @@ const jwt = require('jsonwebtoken');
 // const bcrypt = require('bcrypt');
 router.get('/login/success', async (req, res) => {
 	try {
-	  if (!req.user) {
-		return res.status(400).json({ message: 'Invalid request' });
-	  }
+	//   if (!req.user) {
+		return res.status(200).json({ message: 'getsuccessfully',user:req.user});
+	//   }
   
-	  const userEmail = req.user.emails.value; // Correctly accessing the email value
-	  let existingUser = await User.findOne({ email: userEmail });
+	//   const userEmail = req.user.emails.value; // Correctly accessing the email value
+	//   let existingUser = await User.findOne({ email: userEmail });
   
-	  if (!existingUser) {
-		console.log(req.user);
+	//   if (!existingUser) {
+	// 	console.log(req.user);
   
-		const newUser = new User({
-		  firstname: req.user.name.givenName,
-		  lastname: req.user.name.familyName,
-		  email: userEmail,
-		  status: true,
-		});
+	// 	const newUser = new User({
+	// 	  firstname: req.user.name.givenName,
+	// 	  lastname: req.user.name.familyName,
+	// 	  email: userEmail,
+	// 	  status: true,
+	// 	});
   
-		jwt.sign({ id: newUser._id }, secretID, { expiresIn: '30d' }, async (err, UserToken) => {
-		  if (err) {
-			return res.status(500).json({ message: 'Token generation failed', errors: err.message });
-		  }
+	// 	jwt.sign({ id: newUser._id }, secretID, { expiresIn: '30d' }, async (err, UserToken) => {
+	// 	  if (err) {
+	// 		return res.status(500).json({ message: 'Token generation failed', errors: err.message });
+	// 	  }
   
-		  newUser.sessionExpiration = new Date().getTime() + (1000 * 60 * 60 * 24 * 30); // 30 days in milliseconds
-		  newUser.jwttoken = UserToken;
-		  await newUser.save();
+	// 	  newUser.sessionExpiration = new Date().getTime() + (1000 * 60 * 60 * 24 * 30); // 30 days in milliseconds
+	// 	  newUser.jwttoken = UserToken;
+	// 	  await newUser.save();
   
-		  return res.status(200).json({ message: 'Successfully signed in', newuser: newUser });
-		});
-	  } else {
-		return res.status(200).json({ newuser: existingUser });
-	  }
+	// 	  return res.status(200).json({ message: 'Successfully signed in', newuser: newUser });
+	// 	});
+	//   } else {
+	// 	return res.status(200).json({ newuser: existingUser });
+	//   }
 	} catch (error) {
 	  return res.status(500).json({ message: 'Internal server error', errors: error.message });
 	}
